e3973b2f6d4aa38dbd62c80b9dbc4d1a266e8621
Philippe Crackau <philippe.crackau@stud.uni-bamberg.de>
2025-07-07 11:50:04 +0200

    added first try object detection

--- pipeline/identifyobjects.py
+++ pipeline/identifyobjects.py
@@ -13,10 +13,15 @@ film_path_lst = find_film_files(DATASETS)
 film_path = film_path_lst[786]   
 frames_output_dir = Path("processing") / film_path.stem / "key_frames"
 
-model = YOLO("yolov8x.pt") 
+model = YOLO("yolov8x.pt")
 results = model(frames_output_dir / "keyFrames" / "keyframe8.jpg")
 for result in results:
-    print(result.boxes)
+    #print(result.boxes)
+    for box in results[0].boxes:
+        class_id = int(box.cls[0])
+        label = results[0].names[class_id]
+        confidence = float(box.conf[0])
+        print(f"Detected: {label} ({confidence:.2%})")
 
 
 
